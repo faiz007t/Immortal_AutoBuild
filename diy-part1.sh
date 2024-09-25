16f57b2dd84fef0fbb55073fb43cf806c9e16574
brianzc0306 <48046657+brianzc0306@users.noreply.github.com>
2024-09-26 06:50:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
-echo 'src-git lucky https://github.com/gdy666/lucky.git' >>feeds.conf.default
