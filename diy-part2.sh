21c5eecc8110c97a0b84b137dc7a4c45a986dc09
brianzc0306 <48046657+brianzc0306@users.noreply.github.com>
2024-10-03 08:21:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,3 +22,4 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
+git clone https://github.com/morytyann/OpenWrt-mihomo.git package/luci-app-mihomo
