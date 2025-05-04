7d0d978b2f9b6a4d35ca67f4e24526e603f2ad57
brianzc0306 <48046657+brianzc0306@users.noreply.github.com>
2025-05-04 20:51:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,9 +10,9 @@
 # See /LICENSE for more information.
 
 #Change the default WAN to eth0
-sed -i "s/net\/eth1/net\/eth0/g" package/base-files/files/etc/board.d/99-default_network
-sed -i "s/wan 'eth1'/wan 'eth0'/g" package/base-files/files/etc/board.d/99-default_network
-sed -i "s/lan 'eth0'/lan 'eth1'/g" package/base-files/files/etc/board.d/99-default_network
+#sed -i "s/net\/eth1/net\/eth0/g" package/base-files/files/etc/board.d/99-default_network
+#sed -i "s/wan 'eth1'/wan 'eth0'/g" package/base-files/files/etc/board.d/99-default_network
+#sed -i "s/lan 'eth0'/lan 'eth1'/g" package/base-files/files/etc/board.d/99-default_network
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
@@ -21,5 +21,5 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
-git clone https://github.com/morytyann/OpenWrt-mihomo.git package/luci-app-mihomo
+#git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
+#git clone https://github.com/morytyann/OpenWrt-mihomo.git package/luci-app-mihomo
